f39d3aa6b9d81c71656f8dc6147798f017b10fe1
Andrew Fitzgerald <fitzgeralda@sureskills.com>
2022-03-31 15:08:42 +0100

    chore: initial commit

---
+++ Data/ActorsRestApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ActorsRestApi.Models;
+
+namespace ActorsRestApi.Data
+{
+    public class ActorsRestApiContext : DbContext
+    {
+        public ActorsRestApiContext (DbContextOptions<ActorsRestApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ActorsRestApi.Models.Actor> Actor { get; set; }
+    }
+}
---
+++ Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ActorsRestApi.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Guid CreatedBy { get; set; }
+    }
+}
